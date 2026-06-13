58b55c94f06c40e47fbe09411881589e4d96e826
zhongsanming <zhongsanming@compiler-dev.com>
2026-06-13 11:35:08 +0800

    update

--- triton-cuda-graph-auto-tune.py
+++ triton-cuda-graph-auto-tune.py
@@ -1,22 +1,4 @@
-"""
-Vector Addition
-===============
-
-In this tutorial, you will write a simple vector addition using Triton.
-
-In doing so, you will learn about:
-
-* The basic programming model of Triton.
-
-* The `triton.jit` decorator, which is used to define Triton kernels.
-
-* The best practices for validating and benchmarking your custom ops against native reference implementations.
-
-"""
-
-# %%
-# Compute Kernel
-# --------------
+"""Vector Addition from triton tutorial, removed most of the comment for clarity"""
 
 import torch
 
@@ -26,82 +8,50 @@ import triton.language as tl
 DEVICE = triton.runtime.driver.active.get_active_torch_device()
 
 
+# add a dead simple autotune config
 @triton.autotune(
     configs=[
         triton.Config({"BLOCK_SIZE": 64}, num_warps=2),
         triton.Config({"BLOCK_SIZE": 128}, num_warps=4),
-        triton.Config({"BLOCK_SIZE": 256}, num_warps=4),
-        triton.Config({"BLOCK_SIZE": 512}, num_warps=8),
-        triton.Config({"BLOCK_SIZE": 1024}, num_warps=8),
     ],
     key=["BLOCK_SIZE"],
+    do_bench=triton.testing.do_bench_cudagraph
 )
 @triton.jit
-def add_kernel(
-    x_ptr,  # *Pointer* to first input vector.
-    y_ptr,  # *Pointer* to second input vector.
-    output_ptr,  # *Pointer* to output vector.
-    n_elements,  # Size of the vector.
-    BLOCK_SIZE: tl.constexpr,  # Number of elements each program should process.
-    # NOTE: `constexpr` so it can be used as a shape value.
-):
-    # There are multiple 'programs' processing different data. We identify which program
-    # we are here:
-    pid = tl.program_id(axis=0)  # We use a 1D launch grid so axis is 0.
-    # This program will process inputs that are offset from the initial data.
-    # For instance, if you had a vector of length 256 and block_size of 64, the programs
-    # would each access the elements [0:64, 64:128, 128:192, 192:256].
-    # Note that offsets is a list of pointers:
+def add_kernel(x_ptr, y_ptr, output_ptr, n_elements, BLOCK_SIZE: tl.constexpr):
+    pid = tl.program_id(axis=0)
     block_start = pid * BLOCK_SIZE
     offsets = block_start + tl.arange(0, BLOCK_SIZE)
-    # Create a mask to guard memory operations against out-of-bounds accesses.
     mask = offsets < n_elements
-    # Load x and y from DRAM, masking out any extra elements in case the input is not a
-    # multiple of the block size.
     x = tl.load(x_ptr + offsets, mask=mask)
     y = tl.load(y_ptr + offsets, mask=mask)
     output = x + y
-    # Write x + y back to DRAM.
     tl.store(output_ptr + offsets, output, mask=mask)
 
 
-# %%
-# Let's also declare a helper function to (1) allocate the `z` tensor
-# and (2) enqueue the above kernel with appropriate grid/block sizes:
-
-
 def add(x: torch.Tensor, y: torch.Tensor):
-    # We need to preallocate the output.
     output = torch.empty_like(x)
     assert x.device == DEVICE and y.device == DEVICE and output.device == DEVICE
     n_elements = output.numel()
-    # The SPMD launch grid denotes the number of kernel instances that run in parallel.
-    # It is analogous to CUDA launch grids. It can be either Tuple[int], or Callable(metaparameters) -> Tuple[int].
-    # In this case, we use a 1D grid where the size is the number of blocks:
     grid = lambda meta: (triton.cdiv(n_elements, meta["BLOCK_SIZE"]),)
-    # NOTE:
-    #  - Each torch.tensor object is implicitly converted into a pointer to its first element.
-    #  - `triton.jit`'ed functions can be indexed with a launch grid to obtain a callable GPU kernel.
-    #  - Don't forget to pass meta-parameters as keywords arguments.
+    # remove fixed BLOCK_SIZE
     add_kernel[grid](x, y, output, n_elements)
-    # We return a handle to z but, since `torch.cuda.synchronize()` hasn't been called, the kernel is still
-    # running asynchronously at this point.
     return output
 
 
-# %%
-# We can now use the above function to compute the element-wise sum of two `torch.tensor` objects and test its correctness:
+if __name__ == "__main__":
+    torch.manual_seed(0)
+    size = 98432
+    x = torch.rand(size, device=DEVICE)
+    y = torch.rand(size, device=DEVICE)
 
-torch.manual_seed(0)
-size = 98432
-x = torch.rand(size, device=DEVICE)
-y = torch.rand(size, device=DEVICE)
+    # use it with cuda graph before any cache exists
+    # the sync call inside do_bench_cudagraph will invalidate the cuda graph capture
+    with torch.cuda.stream(torch.cuda.Stream()):
+        g = torch.cuda.CUDAGraph()
+        with torch.cuda.graph(g):
+            add(x, y)
+        torch.cuda.synchronize()
 
-with torch.cuda.stream(torch.cuda.Stream()):
-    g = torch.cuda.CUDAGraph()
-    with torch.cuda.graph(g):
-        add(x, y)
-    torch.cuda.synchronize()
-
-    g.replay()
-    torch.cuda.synchronize()
+        g.replay()
+        torch.cuda.synchronize()
